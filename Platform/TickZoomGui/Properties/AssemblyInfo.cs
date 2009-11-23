0f2e8373d5bf6b780f4c0214137999a817fd3951
AutoBuild <wayne@tickzoom.com>
2009-11-23 17:39:47 -0500

    Build server updated version to 0.5.22.905.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.5.22.740")]
+[assembly: AssemblyVersion("0.5.22.905")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
